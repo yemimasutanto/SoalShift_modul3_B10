16fdd2b3e5472039b2525e17ff5eff3917b62377
bayulaxana <bynd4spd@gmail.com>
2019-04-03 01:52:50 +0700

    Fixing No. 1

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -1,36 +1,89 @@
+/* Menampilkan faktorial secara berurutan */
+#define SISOP B10
 #include<stdio.h>
 #include<string.h>
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
-typedef long long int ll;
+typedef long long int LL;
 
-void *factorial(void *argv){
-    ll a=strtol((char *)argv,NULL,10);//convert string to long long int
-    ll num=1;
-    for(ll i=1;i<=a;i++) num*=i;
-    printf("Hasil %lld! = %lld\n", a, num);
-    return NULL;
-}
+LL fact[100];
 
-int main(int argc,char *argv[]){
-    ll idx,j,i,temp;
+void *find_fact(void* argv);
+void Sort(LL *arr, int n);
+void printResult(LL *arr, int n);
+
+int main(int argc, char *argv[]) {
+    LL num[argc-1];
     pthread_t tid[argc-1];
-    for(i=0; i<(argc-1); i++){
-        idx=i;
-        for(j=i+1; j<argc; j++){
-            if(*argv[j]<*argv[idx]){
-                idx=j;
-            }
+    
+    for(int i=1; i<argc; i++) {
+        num[i-1] = atoll(argv[i]);
+    }
+    Sort(num, argc-1);
+    for(int i=0; i<argc-1; i++) {
+        LL *x = malloc(sizeof(*x));
+        *x = num[i];
+        pthread_create(&(tid[i]), NULL, find_fact, x);
+    }
+    for(int i=0; i<argc-1; i++) pthread_join(tid[i], NULL);
+    printResult(num, argc-1);
+    return 0;
+}
+
+void *find_fact(void* argv) {
+    LL num = *((long long*)argv);
+    fact[0] = fact[1] = 1;
+    for(int i=2; i<=num; i++) fact[i] = i * fact[i-1];
+}
+
+void printResult(LL *arr, int n) {
+    for(int i=0; i<n; i++)
+        printf("%lld! = %lld\n",arr[i], fact[arr[i]]);
+}
+
+void Sort(LL *arr, int n) {
+    LL key; int i, j;
+    for(i=1; i<n; i++) {
+        key = arr[i];
+        j = i-1;
+
+        while(j>=0 && arr[j] > key) {
+            arr[j+1] = arr[j];
+            j--;
         }
-        temp=*argv[i];
-        *argv[i]=*argv[idx];
-        *argv[idx]=temp;
-    }
-    for(int i=0;i<argc-1;i++){
-        pthread_create(&(tid[i]),NULL,&factorial,(void *)argv[i+1]);
-    }
-    for(int i=0;i<argc-1;i++){
-        pthread_join(tid[i],NULL);
+        arr[j+1] = key;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* SISOP B10 */
